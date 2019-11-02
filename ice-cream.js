b5d16a143e199b3f6169e00ee4148f0958c932e3
Julie Nguyen <julie-nguyen@live.com>
2019-11-02 17:58:29 +0000

    ice-cream.js

---
+++ ice-cream.js
@@ -0,0 +1,11 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+let newIceCreamFlavors = iceCreamFlavours;
+newIceCreamFlavors.push('root beer');
+console.log(newIceCreamFlavors);
+
+console.log(newIceCreamFlavors[0]);
+console.log(newIceCreamFlavors[-1]);
